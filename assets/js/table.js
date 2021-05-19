7cc0de7b83538667174fd0bc8bffbfebef748229
renzobarrionuevo <renzobarrionuevo@gmail.com>
2021-05-19 19:27:21 -0300

    select pair

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,7 +1,25 @@
+$.get( "http://79.157.14.252:7000/pairs", function( data ) {
+    var str=''; // variable to store the options
+
+    // var pair = new Array("WETH/MILF","February","March","April","May","June","July","August",
+    // "September","October","November","December");
+    var pair = data.values;
+    for (var i=0; i < pair.length;++i){
+    str += '<option value="'+pair[i].replace('-', '/')+'" />'; // Storing options in variable
+    }
+    var my_list=document.getElementById("pairs");
+    my_list.innerHTML = str;
+})
+
+function getData(obj){
+    if(obj.value != '') {
+        setpairs(obj.value.split("/")[0], obj.value.split("/")[1])
+        obj.value = ''
+    }   
+    }
+
 
 $.get( "http://79.157.14.252:7000/last_txns", function( data ) {
-
-    console.log(data)
 
     const txs = data.values;
 
@@ -12,15 +30,8 @@ $.get( "http://79.157.14.252:7000/last_txns", function( data ) {
     const $tr = document.createElement("tr");
 
     let $tdnames = document.createElement("td");
-    $tdnames.textContent = (tx.token_used_for_payment_symbol + '/' + tx.token_being_bought_symbol) ;
+    $tdnames.textContent = (tx.token_being_bought_symbol + '/' + tx.token_used_for_payment_symbol) ;
     $tr.appendChild($tdnames);
-
-    let $tdtokenp= document.createElement("td");
-    var tokenpy = tx.tokens_payed_for_the_exchange_normalized;
-    tokenpy = parseFloat(tokenpy).toFixed(4)
-    $tdtokenp.textContent = (tokenpy);
-    $tr.appendChild($tdtokenp);
-    $tdtokenp.style.textAlign = "right" 
 
     let $tdtokenb = document.createElement("td");
     var tokend = tx.tokens_bought_from_the_exchange_normalized;
@@ -28,6 +39,13 @@ $.get( "http://79.157.14.252:7000/last_txns", function( data ) {
     $tdtokenb.textContent = (tokend) ;
     $tr.appendChild($tdtokenb);
     $tdtokenb.style.textAlign = "right" 
+
+    let $tdtokenp= document.createElement("td");
+    var tokenpy = tx.tokens_payed_for_the_exchange_normalized;
+    tokenpy = parseFloat(tokenpy).toFixed(4)
+    $tdtokenp.textContent = (tokenpy);
+    $tr.appendChild($tdtokenp);
+    $tdtokenp.style.textAlign = "right" 
 
     let $tdtx_hash = document.createElement("td");
     $tdtx_hash.textContent = tx.tx_hash; 
@@ -53,66 +71,91 @@ $.get( "http://79.157.14.252:7000/last_txns", function( data ) {
 
 });
 
-$.get( "http://79.157.14.252:7000/txns/MILF-WETH", function( data ) {
+function setpairs(token1, token2) {
 
-    console.log(data)
+    $("#buyTable").empty()
+    $("#sellTable").empty()
+
+
+
+$.get( "http://79.157.14.252:7000/txns/" + token1 + '-' + token2, function( data ) {
+
+    document.getElementById("title-1").innerHTML = "SWAP " + token1 + "/" + token2 
+    document.getElementById("span-1").innerHTML = token1 + "/" + token2 
+    document.getElementById("amount-1").innerHTML = "(" + token1 + ")"
+    document.getElementById("title-2").innerHTML = "SWAP " + token2 + "/" + token1 
+    document.getElementById("span-2").innerHTML = token2 + "/" + token1 
+    document.getElementById("amount-2").innerHTML = "(" + token2 + ")"
+    document.getElementById("price-1").innerHTML =  token1 
+    document.getElementById("price-2").innerHTML = token2
+    document.getElementById("price-1-2").innerHTML =  token1 
+    document.getElementById("price-2-2").innerHTML = token2  
+
 
     const buy = data.values.reverse().slice(0,5);
 
     const $buyTable = document.querySelector("#buyTable");
     buy.forEach(buy => {
-    //<tr>
-    // if(token_being_bought_symbol)
-    const $tr = document.createElement("tr");
+        //<tr>
+        if(buy.status != 0) {
+            const $tr = document.createElement("tr");
 
-    let $tdprice = document.createElement("td");
-    var price = parseFloat(buy.tokens_payed_for_the_exchange_normalized / buy.tokens_bought_from_the_exchange_normalized).toFixed(8);
-    $tdprice.textContent = price;
-    $tdprice.style.color = "greenyellow" 
-    $tr.appendChild($tdprice);
+            let $tdprice = document.createElement("td");
+            var price = parseFloat(buy.tokens_payed_for_the_exchange_normalized / buy.tokens_bought_from_the_exchange_normalized).toFixed(8);
+            $tdprice.textContent = price;
+            $tdprice.style.color = "greenyellow" 
+            $tr.appendChild($tdprice);
 
-    let $tdtotal = document.createElement("td");
-    $tdtotal.textContent = parseFloat(buy.tokens_payed_for_the_exchange_normalized).toFixed(8); 
-    $tr.appendChild($tdtotal);
+            let $tdtotal = document.createElement("td");
+            $tdtotal.textContent = parseFloat(buy.tokens_bought_from_the_exchange_normalized).toFixed(8); 
+            $tr.appendChild($tdtotal);
 
-    let $tdtokenbo = document.createElement("td");
-    $tdtokenbo.textContent = parseFloat(buy.tokens_bought_from_the_exchange_normalized).toFixed(8); 
-    $tr.appendChild($tdtokenbo);
-    // <tr
-    $buyTable.appendChild($tr);
+            let $tdtokenbo = document.createElement("td");
+            $tdtokenbo.textContent = parseFloat(buy.tokens_payed_for_the_exchange_normalized).toFixed(8); 
+            $tr.appendChild($tdtokenbo);
+            // <tr
+            $buyTable.appendChild($tr);
 
+
+        }
     });
 
 });
 
-$.get( "http://79.157.14.252:7000/txns/WETH-MILF", function( data ) {
-
-    console.log(data)
+$.get( "http://79.157.14.252:7000/txns/" + token2 + '-' + token1 , function( data ) {
 
     const sell = data.values.reverse().slice(0,5);
 
     const $sellTable = document.querySelector("#sellTable");
     sell.forEach(sell => {
-    //<tr>
-    // if(token_being_bought_symbol)
-    const $tr = document.createElement("tr");
+        
+        if(sell.status != 0) {
+            //<tr>
+            // if(token_being_bought_symbol)
+            const $tr = document.createElement("tr");
 
-    let $tdprice = document.createElement("td");
-    var price = parseFloat(sell.tokens_payed_for_the_exchange_normalized / sell.tokens_bought_from_the_exchange_normalized).toFixed(8);
-    $tdprice.textContent = price;
-    $tdprice.style.color = "red" 
-    $tr.appendChild($tdprice);
+            let $tdprice = document.createElement("td");
+            var price = parseFloat(sell.tokens_payed_for_the_exchange_normalized / sell.tokens_bought_from_the_exchange_normalized).toFixed(8);
+            $tdprice.textContent = price;
+            $tdprice.style.color = "red" 
+            $tr.appendChild($tdprice);
 
-    let $tdtokenbo = document.createElement("td");
-    $tdtokenbo.textContent = parseFloat(sell.tokens_bought_from_the_exchange_normalized).toFixed(8); 
-    $tr.appendChild($tdtokenbo);
+            let $tdtokenbo = document.createElement("td");
+            $tdtokenbo.textContent = parseFloat(sell.tokens_bought_from_the_exchange_normalized).toFixed(8); 
+            $tr.appendChild($tdtokenbo);
 
-    let $tdtotal = document.createElement("td");
-    $tdtotal.textContent = parseFloat(sell.tokens_payed_for_the_exchange_normalized).toFixed(8); 
-    $tr.appendChild($tdtotal);
-    // <tr
-    $sellTable.appendChild($tr);
-
-    });
+            let $tdtotal = document.createElement("td");
+            $tdtotal.textContent = parseFloat(sell.tokens_payed_for_the_exchange_normalized).toFixed(8); 
+            $tr.appendChild($tdtotal);
+            // <tr
+            $sellTable.appendChild($tr);
+            }
+        });
 
 });
+
+}
+
+
+
+setpairs("WETH", "MILF")
