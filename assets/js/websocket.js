d62e92f6246794656677b1c27ccae464c9bcdd2b
Guillermo <guillermo.paoletti@gmail.com>
2021-06-08 15:52:42 +0200

    Websockets for mempool

--- assets/js/websocket.js
+++ assets/js/websocket.js
@@ -1,13 +1,4 @@
-function add_to_buy(element) {
-    return add_to_table("#buyTable", element, "#77dd77");
-}
-
-function add_to_sell(element) {
-    return add_to_table("#sellTable", element, "#c23b22");
-}
-
-
-//PERDON GUILLE POR ESTE DESASTRE PERO HABIA QUE AGREGAR UNA LINEA Y CAMBIABAN UNOS VALORES Y NO SABIA COMO HACERLO SINO
+// Mempool
 function add_to_mempool_bids(element) {
     return add_to_mempool("#mempool_bids", element, "#77dd77");
 }
@@ -16,64 +7,55 @@ function add_to_mempool_asks(element) {
     return add_to_mempool("#mempool_asks", element, "#c23b22");
 }
 
-function add_to_mempool_bids(tabla_id, element, color) {
+function add_to_mempool(tabla_id, element, color) {
     const table = document.querySelector(tabla_id);
     if (element.status != 0) {
         const $tr = table.insertRow(0);
         let $tdprice = document.createElement("td");
-        var price = decimals(parseFloat(
-            mempool.token_in_normalized / mempool.token_out_normalized
-        ));
+        let $tdtotal = document.createElement("td");
+        let $tdtokenbo = document.createElement("td");
+
+        var price;
+        if (tabla_id == "#mempool_bids") {
+            price = decimals(parseFloat(
+                mempool.token_in_normalized / mempool.token_out_normalized
+            ));
+            $tdtotal.textContent = decimals(parseFloat(element.token_out_normalized));
+            $tdtokenbo.textContent = decimals(parseFloat(element.token_in_normalized));
+        } else {
+            price = decimals(parseFloat(
+                mempool.token_out_normalized / mempool.token_in_normalized
+            ));
+            $tdtotal.textContent = decimals(parseFloat(element.token_in_normalized));
+            $tdtokenbo.textContent = decimals(parseFloat(element.token_out_normalized));
+        }
+        // Price
         $tdprice.textContent = price;
         $tdprice.style.color = color;
         $tr.appendChild($tdprice);
 
-        let $tdtotal = document.createElement("td");
-        $tdtotal.textContent = decimals(parseFloat(element.token_out_normalized));
+        // Total
         $tr.appendChild($tdtotal);
-        
 
-        let $tdtokenbo = document.createElement("td");
-        $tdtokenbo.textContent = decimals(parseFloat(element.token_in_normalized));
+        // Tokenbo
         $tr.appendChild($tdtokenbo);
         $tdtokenbo.style.textAlign = "right";
 
-        var rowCount = table.rows.length;
-        if (rowCount > 25) {
-            table.deleteRow(rowCount - 1);
+        while (table.rows.length > 25) {
+            table.deleteRow(table.rows.length - 1);
         }
     }
 }
 
-function add_to_mempool_asks(tabla_id, element, color) {
-    const table = document.querySelector(tabla_id);
-    if (element.status != 0) {
-        const $tr = table.insertRow(0);
-        let $tdprice = document.createElement("td");
-        var price = decimals(parseFloat(
-            mempool.token_out_normalized / mempool.token_in_normalized
-        ));
-        $tdprice.textContent = price;
-        $tdprice.style.color = color;
-        $tr.appendChild($tdprice);
 
-        let $tdtotal = document.createElement("td");
-        $tdtotal.textContent = decimals(parseFloat(element.token_in_normalized));
-        $tr.appendChild($tdtotal);
-
-        let $tdtokenbo = document.createElement("td");
-        $tdtokenbo.textContent = decimals(parseFloat(element.token_out_normalized));
-        $tr.appendChild($tdtokenbo);
-        $tdtokenbo.style.textAlign = "right";
-
-        var rowCount = table.rows.length;
-        if (rowCount > 25) {
-            table.deleteRow(rowCount - 1);
-        }
-    }
+// Last swaps
+function add_to_buy(element) {
+    return add_to_table("#buyTable", element, "#77dd77");
 }
 
-//ESTA ACA METO LA PATA NOMAS ABAJO NO ENTENDI COMO PONERLO DONDE ESTA EL WSEndPOint sin romper lo que está ahora
+function add_to_sell(element) {
+    return add_to_table("#sellTable", element, "#c23b22");
+}
 
 function add_to_table(tabla_id, element, color) {
     const table = document.querySelector(tabla_id);
@@ -92,9 +74,8 @@ function add_to_table(tabla_id, element, color) {
         $tr.appendChild($tdtotal);
         $tdtotal.style.textAlign = "right";
 
-        var rowCount = table.rows.length;
-        if (rowCount > 25) {
-            table.deleteRow(rowCount - 1);
+        while (table.rows.length > 25) {
+            table.deleteRow(table.rows.length - 1);
         }
     }
 }
@@ -102,7 +83,7 @@ function add_to_table(tabla_id, element, color) {
 async function add_to_last_transactions(tx) {
     const $cuerpoTabla = document.querySelector("#cuerpoTabla");
     let img_token_out = localStorage.getItem(tx.token_out);
-    
+
     if (img_token_out == null) {
         let url_Req =
             "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/" +
@@ -172,12 +153,15 @@ async function add_to_last_transactions(tx) {
         $tdtx_hash.style.textAlign = "right";
 
     }
+
+    while (cuerpoTabla.rows.length > 50) {
+        cuerpoTabla.deleteRow(cuerpoTabla.rows.length - 1);
+    }
 }
 
 
 // var apiEndPoint = "test.hanchon.live/api"
 var wsEndPoint = "test.hanchon.live/api";
-console.log();
 
 const ws = new WebSocket(`wss://${wsEndPoint}/ws`);
 ws.onmessage = async (e) => {
@@ -185,11 +169,19 @@ ws.onmessage = async (e) => {
     t2 = document.getElementById("token2").value;
 
     data = JSON.parse(e.data);
-    if (data.pair_in == t1 && data.pair_out == t2) {
-        add_to_sell(JSON.parse(data.value));
-    } else if (data.pair_in == t2 && data.pair_out == t1) {
-        add_to_buy(JSON.parse(data.value));
-    }
+    if (data.type == 'block') {
+        if (data.pair_in == t1 && data.pair_out == t2) {
+            add_to_sell(JSON.parse(data.value));
+        } else if (data.pair_in == t2 && data.pair_out == t1) {
+            add_to_buy(JSON.parse(data.value));
+        }
 
-    await add_to_last_transactions(JSON.parse(data.value));
+        await add_to_last_transactions(JSON.parse(data.value));
+    } else if (data.type == 'mempool') {
+        if (data.pair_in == t1 && data.pair_out == t2) {
+            add_to_mempool_asks(JSON.parse(data.value));
+        } else if (data.pair_in == t2 && data.pair_out == t1) {
+            add_to_mempool_bids(JSON.parse(data.value));
+        }
+    }
 };
